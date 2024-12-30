e23b97d22a1dac5886ffcb57a4d1d528d04dd4fe
aszurita <nzuritag@fiec.espol.edu.ec>
2024-12-30 16:22:41 -0500

    Proyecto Base, Proyecto de IA, sobre predicción de PNEUMONIA

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,69 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, signal, ViewChild, ElementRef } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonCard, IonCardContent, IonButton, IonList, IonItem, IonLabel, } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-
+/* Importe la función y el ícono */
+import { addIcons } from 'ionicons';
+import { cloudUploadOutline } from 'ionicons/icons';
+import { TeachablemachineService } from '../services/teachablemachine.service';
+import { PercentPipe } from '@angular/common';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFab, IonFabButton, IonIcon, IonCard, IonCardContent, IonButton, IonList, IonItem, IonLabel, PercentPipe],
 })
 export class Tab1Page {
-  constructor() {}
+  imageReady = signal(false)
+  imageUrl = signal("")
+  /* Declare la referencia al elemento con el id image */
+  @ViewChild('image', { static: false }) imageElement!: ElementRef<HTMLImageElement>;
+  predictions: any[] = [];
+
+  /* El método onSubmit para enviar los datos del formulario mediante el servicio */
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+
+      const reader = new FileReader();
+
+      // Convertir el archivo a una URL base64 para mostrarlo en el html
+      // Convertir el archivo a una URL base64 para mostrarlo en el html
+      reader.onload = () => {
+        this.imageUrl.set(reader.result as string)
+        this.imageReady.set(true)
+      };
+
+      reader.readAsDataURL(file); // Leer el archivo como base64
+    }
+  }
+
+  /* Declare los atributos para almacenar el modelo y la lista de clases */
+  modelLoaded = signal(false);
+  classLabels: string[] = [];
+
+  constructor(private teachablemachine: TeachablemachineService) {
+    addIcons({ cloudUploadOutline });
+  }
+
+  /* Método ngOnInit para cargar el modelo y las clases */
+  async ngOnInit() {
+    await this.teachablemachine.loadModel()
+    this.classLabels = this.teachablemachine.getClassLabels()
+    this.modelLoaded.set(true)
+  }
+
+  /* Método para obtener la predicción a partir de la imagen */
+  async predict() {
+    try {
+        const image = this.imageElement.nativeElement;
+        this.predictions = await this.teachablemachine.predict(image);
+    } catch (error) {
+        console.error(error);
+        alert('Error al realizar la predicción.');
+    }
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { firebaseConfig } from './credentials';
 import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app.component'; 
+/* Importe los módulos de AngularFire */
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    /* Inyecte los módulos de AngularFire */
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ],
 });
