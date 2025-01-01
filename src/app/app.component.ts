828a80909ba0289633df132c03d0408a3c30b1c9
aszurita <nzuritag@fiec.espol.edu.ec>
2025-01-01 17:46:01 -0500

    Home Terminado, Boton continuar redirige a la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonApp, IonRouterOutlet, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  styleUrls: ['app.component.scss'],
+  imports: [CommonModule, IonApp, IonRouterOutlet, IonContent, IonButton],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  showSplash = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // If you want to show splash screen only on first visit,
+    // you can check localStorage here
+    const hasVisited = localStorage.getItem('hasVisited');
+    if (hasVisited) {
+      this.showSplash = false;
+    }
+  }
+
+  navigateToTabs() {
+    this.showSplash = false;
+    localStorage.setItem('hasVisited', 'true');
+    this.router.navigate(['/tabs']);
+  }
 }
